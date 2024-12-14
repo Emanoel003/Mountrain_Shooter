b9a3726c23f9184ba3851f7033399e7195dbb3a1
Emanoel003 <aquinos.emanuel@gmail.com>
2024-12-14 16:59:03 -0300

    Update Enemy.py

--- code/Enemy.py
+++ code/Enemy.py
@@ -9,9 +9,17 @@ class Enemy(Entity):
     def __init__(self, name: str, position: tuple):
         super().__init__(name, position)
         self.shot_delay = ENTITY_SHOT_DELAY[self.name]
+        self.vertical_speed = ENTITY_SPEED[self.name]  # Velocidade vertical inicial
 
     def move(self):
         self.rect.centerx -= ENTITY_SPEED[self.name]
+
+        if self.name == 'Enemy3':
+            self.rect.centery += self.vertical_speed
+            if self.rect.top <= 0:
+                self.vertical_speed = abs(self.vertical_speed) * 2  # Dobra a velocidade ao bater no topo
+            elif self.rect.bottom >= self.rect.height:
+                self.vertical_speed = -abs(self.vertical_speed)  # Inverte a direção ao bater na parte inferior
 
     def shoot(self):
         self.shot_delay -= 1
